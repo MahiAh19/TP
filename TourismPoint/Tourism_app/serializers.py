cc76342c243973b5bd687b3f5255d2ea6547f14a
Mahima Ahsan <mahima.ahsan19@gmail.com>
2023-08-08 09:51:53 +0600

    added serializers

---
+++ TourismPoint/Tourism_app/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Customer, Queries
+
+class CutomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['id', 'name', 'phone_number', 'email']
+        read_only_fields = ['id']
+
+class QueriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Queries 
+        fields = ['id', 'destination', 'departure', 'date_of_travel', 'customer']
+        read_only_fields = ['id']
